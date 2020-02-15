441abc24b9bc6dfdeb1f59c46e0952a8a7d0e0c9
Guy Wilson <wilson.guy@gmail.com>
2020-02-15 13:00:50 +0000

    Added helper files from weatherctl

---
+++ src/main.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pthread.h>
+#include <time.h>
+#include <signal.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <clk.h>
+
+using namespace std;
+
+void printUsage(char * pszAppName)
+{
+	printf("\n Usage: %s [OPTIONS]\n\n", pszAppName);
+	printf("  Options:\n");
+	printf("   -h/?             Print this help\n");
+	printf("   -version         Print the program version\n");
+	printf("   -cfg configfile  Specify the cfg file, default is ./webconfig.cfg\n");
+	printf("   -d               Daemonise this application\n");
+	printf("\n");
+}
+
+void daemonise()
+{
+	pid_t			pid;
+	pid_t			sid;
+
+	fprintf(stdout, "Starting daemon...\n");
+	fflush(stdout);
+
+	do {
+		pid = fork();
+	}
+	while ((pid == -1) && (errno == EAGAIN));
+
+	if (pid < 0) {
+		fprintf(stderr, "Forking daemon failed...\n");
+		fflush(stderr);
+		exit(EXIT_FAILURE);
+	}
+	if (pid > 0) {
+		fprintf(stdout, "Exiting child process...\n");
+		fflush(stdout);
+		exit(EXIT_SUCCESS);
+	}
+
+	sid = setsid();
+	
+	if(sid < 0) {
+		fprintf(stderr, "Failed calling setsid()...\n");
+		fflush(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	signal(SIGCHLD, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);    
+	
+	umask(0);
+
+	if((chdir("/") == -1)) {
+		fprintf(stderr, "Failed changing directory\n");
+		fflush(stderr);
+		exit(EXIT_FAILURE);
+	}
+	
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+}
+
+int main(int argc, char *argv[])
+{
+	char *			pszAppName;
+//	char *			pszLogFileName = NULL;
+	char *			pszConfigFileName = NULL;
+	char			szPidFileName[PATH_MAX];
+	int				i;
+	bool			isDaemonised = false;
+	bool			isDumpConfig = false;
+	char			cwd[PATH_MAX];
+	
+	pszAppName = strdup(argv[0]);
+
+	getcwd(cwd, sizeof(cwd));
+	
+	strcpy(szPidFileName, cwd);
+	strcat(szPidFileName, "/clksrv.pid");
+
+	printf("\nRunning %s from %s\n", pszAppName, cwd);
+
+	if (argc > 1) {
+		for (i = 1;i < argc;i++) {
+			if (argv[i][0] == '-') {
+				if (argv[i][1] == 'd') {
+					isDaemonised = true;
+				}
+				else if (strcmp(&argv[i][1], "cfg") == 0) {
+					pszConfigFileName = strdup(&argv[++i][0]);
+				}
+				else if (strcmp(&argv[i][1], "-dump-config") == 0) {
+					isDumpConfig = true;
+				}
+				else if (argv[i][1] == 'h' || argv[i][1] == '?') {
+					printUsage(pszAppName);
+					return 0;
+				}
+				else if (strcmp(&argv[i][1], "version") == 0) {
+					printf("%s Version: [%s], Build date: [%s]\n\n", pszAppName, getVersion(), getBuildDate());
+					return 0;
+				}
+				else {
+					printf("Unknown argument '%s'", &argv[i][0]);
+					printUsage(pszAppName);
+					return 0;
+				}
+			}
+		}
+	}
+	else {
+		printUsage(pszAppName);
+		return -1;
+	}
+
+	if (isDaemonised) {
+		daemonise();
+	}
+
+    return 0;
+}
